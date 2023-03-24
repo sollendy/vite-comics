c7eb0d17bb9cc37f10fdeb0e29501300be09cfa9
sollendy <edlannino@tutanota.com>
2023-03-24 17:21:26 +0100

    problemi con immagini

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,10 +1,36 @@
 <script>
-
+    export default {
+    data() {
+        return {
+            oggetti: [
+            {
+                img: "/img/buy-comics-digital-comics.png",
+                text: "DIGITAL COMICS",
+            },
+            
+            {
+                img: "/img/buy-comics-merchandise.png",
+                text: "DC MERCHANDISE",
+            }
+        ],
+        }
+    }
+}
 </script>
 
 <template>
     <div id="corpo-pagina">
     contenuto immenso di questo fantastico sito
+    </div>
+    <div id="barra-celeste">
+        <ul>
+            <li v-for="oggetto in oggetti">
+                <div>
+                    {{ oggetto.img }}
+                </div>
+                <span>{{ oggetto.text }}</span>
+            </li>
+        </ul>
     </div>
 </template>
 
@@ -13,5 +39,12 @@
         background-color: black;
         color: white;
         padding: 50px;
+    }
+    #barra-celeste {
+        background-color: rgb(0, 78, 146);
+        color: white;
+        ul {
+            display: flex;
+        }
     }
 </style>
