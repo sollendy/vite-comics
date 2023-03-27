8513b3afabc4182bc40cc6748009206d253d178f
sollendy <edlannino@tutanota.com>
2023-03-27 16:39:32 +0200

    esercizio in corso. problemi con dimensioni immagini

---
+++ src/components/AppAlbi.vue
@@ -0,0 +1,35 @@
+<script>
+    export default {
+        name: "AppAlbi",
+        data() {
+
+        },
+        props: {
+            img: String,
+            prezzo: Number,
+            serie: String,
+            tipologia: String,
+        },
+    }
+</script>
+
+<template>
+
+    <div class="albi">
+        <img :src="img" alt="fumetto">
+        <!-- <div class="prezzo">{{prezzo}}</div> -->
+        <div class="serie">{{serie}}</div>
+        <!-- <div class="tipologia">{{tipologia}}</div> -->
+    </div>
+
+</template>
+
+<style lang="scss">
+    .albi {
+        width: 150px;
+        padding: 10px;
+        img {
+            width: calc(100% );
+        }
+    }
+</style>
